b7313bdb7327633aabb8c892809137cc49ac897c
Arielle Chen & Brian Kennedy <pair+achen116.briankennedy1@devbootcamp.com>
2015-06-11 11:34:55 -0700

    set up user association with tweets

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   validates :username, :presence => true, :uniqueness => true
   validates :email, :presence => true, :uniqueness => true
   validates :password, :presence => true
+
+  has_many :tweets
 end
