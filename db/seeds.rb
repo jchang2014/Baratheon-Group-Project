d55cf237fba086cbe2386d2c68226b3f5f2a0e69
Michael Farr & Jeff Chang <pair+imikie.jchang2014@devbootcamp.com>
2015-06-11 12:06:11 -0700

    Update seeds to create tweets

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@ arielle = User.create!(username: "ac", email: "ac1@gmail.com", password: "ac123"
 
 Tweet.create!(user_id: 1, content: "Hi I'm Brian, and I'm tweeting.")
 Tweet.create!(user_id: 1, content: "This is Brian's second tweet.")
-Tweet.create!(user_id: 2, content: "Hi I'm Brian, and I'm tweeting.")
-Tweet.create!(user_id: 2, content: "Hi Yo great revoker, and I'm tweeting.")
+Tweet.create!(user_id: 2, content: "Hi I'm Arielle, and I'm tweeting.")
+Tweet.create!(user_id: 2, content: "This is Arielle's second tweet.")
