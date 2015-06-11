2d3c554df83249ea45f04622cf20928575ace9a3
Arielle Chen & Brian Kennedy <pair+achen116.briankennedy1@devbootcamp.com>
2015-06-11 11:25:31 -0700

    add route for login to authenticate user and add styling

--- app/controllers/login.rb
+++ app/controllers/login.rb
@@ -1,7 +1,17 @@
+enable :sessions
+
 get '/' do
   erb :login
 end
 
 post '/' do
-
+  user = User.find_by(username: params[:username])
+  if user && (user.password == params[:password])
+    session[:username] = user.username
+    session[:user_id] = user.id
+    redirect '/home' #route tbd
+  else
+    @error = "Username or password invalid."
+    erb :login
+  end
 end
