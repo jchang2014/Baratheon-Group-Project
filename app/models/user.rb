7970367d4e0c921952b64053d5a66b032949f2ec
Arielle Chen & Brian Kennedy <pair+achen116.briankennedy1@devbootcamp.com>
2015-06-11 11:09:37 -0700

    start on login controller and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   validates :username, :presence => true, :uniqueness => true
-  validates :email, :presence => true
+  validates :email, :presence => true, :uniqueness => true
   validates :password, :presence => true
 end
