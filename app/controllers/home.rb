e1119883b0a5328c59d9117a0c6c7b69999a78fe
Michael Farr & Jeff Chang <pair+imikie.jchang2014@devbootcamp.com>
2015-06-11 17:40:11 -0700

    Prepared route for home

--- app/controllers/home.rb
+++ app/controllers/home.rb
@@ -18,3 +18,10 @@ put '/home' do
 
 end
 
+def get_tweets_from_those_im_following (user)
+  those_im_following = user.followings
+  those_im_following.each do |followed|
+      interesting << @tweets.where(user_id: followed.id)
+    end
+  end
+end
